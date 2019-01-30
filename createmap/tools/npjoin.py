4ba8ed1397b9681866e42ecaade4f8f38f26a502
Jeremy Castagno <jdcasta@umich.edu>
2019-01-29 22:31:26 -0500

    Add scaling and axis reordering

--- createmap/tools/npjoin.py
+++ createmap/tools/npjoin.py
@@ -15,6 +15,8 @@ def add_parser(subparser):
 
     parser.add_argument('--files', '-f', type=argparse.FileType('rb'), nargs='+', help='Files to combines')
     parser.add_argument("--las", "-l", help="Convert to las", action="store_true", default=False)
+    parser.add_argument("--scale", "-s", type=str, default="", help="Scale axes, -s 1,1,100 scales column z axes by 100")
+    parser.add_argument("--axis", "-a", type=str, default="", help="Reorder axes, -o 2,1,0 puts swaps x and z axes")
     parser.add_argument("--out", "-o", type=str, default="combined.npy",
                         help="path to output numpy file")
 
@@ -33,6 +35,24 @@ def main(args):
             combined_data = np.vstack((combined_data, data))
         else:
             combined_data = data
+
+
+    # Make modifications
+    if args.scale:
+        split = args.scale.split(',')
+        assert len(split) == combined_data.shape[1]
+        temp = combined_data.copy()
+        for axis, mult_str in enumerate(split):
+            mult_float = float(mult_str)
+            combined_data[:, axis] = temp[:, axis] * mult_float
+    
+    if args.axis:
+        split = args.axis.split(',')
+        assert len(split) == combined_data.shape[1]
+        temp = combined_data.copy()
+        for orig_axis, axis_str in enumerate(split):
+            axis_num = int(axis_str)   
+            combined_data[:, orig_axis] = temp[:, axis_num]
 
     print("Final size is : {}".format(combined_data.shape))
     if args.las:
